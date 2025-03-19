a5481b2096b2adbb43408f5a747acc6b641c7811
Neo Jie Ming, Kin <142193380+kinneo@users.noreply.github.com>
2025-03-19 12:50:11 +0800

    Update motorControl.c
    Updated motor code so now it only uses 4 PWMs and no GPIO pins
    
    still fixing IN1 and IN2 for forward (01) and reverse (10)
    need to edit curve turns to input wuming's angle, for now i js put as x

--- motorControl.c
+++ motorControl.c
@@ -11,6 +11,13 @@
 #define brake 8
 #define x 5 //minus speed on the respective side when doing curved turns
 
+// 7500 = Full duty cycle (HIGH, IN1 = 1, IN2 = 0 for Forward)
+// 0 = No duty cycle (LOW, IN1 = 0, IN2 = 1 for Reverse)
+#define PWM_MAX 7500
+#define PWM_HIGH PWM_MAX
+#define PWM_LOW 0
+
+
 // steps to initialize PWM on KL25Z
 // 1. Enable clock for TPM (Timer/PWM Module)
 // 2. Configure PWM Pins
@@ -18,44 +25,44 @@
 // 4. Configure PWM Duty Cycle
 // 5. Enable TPM Counter
 
-// PTB0 -> TPM1 Channel 0
-// PTB1 -> TPM1 Channel 1
-// PTB2 -> TPM2 Channel 0
-// PTB3 -> TPM2 Channel 1
+// PTB0 -> TPM1 Channel 0 -> Left Motor Speed (FL & BL)
+// PTB1 -> TPM1 Channel 1 -> Right Motor Speed (FR & BR)
+// PTB2 -> TPM2 Channel 0 -> Left Motor Direction (IN1 & IN2)
+// PTB3 -> TPM2 Channel 1 -> Right Motor Direction (IN1 & IN2)
 
 void initMotor(){
 	
 	// 1. Enable clock gating for TPM (Timer/PWM Module)
-	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK; // TPM1
-	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK; // TPM2
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; // Port B
-	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK; // Port D
+	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK; // TPM1 for speed
+	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK; // TPM2 for direction
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; // Port B for PWM
+	// SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK; // Port D
 	
 	// 2. Configure PWM Pins
 	PORTB->PCR[0] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[0] |= PORT_PCR_MUX(3); // enable ALT3 (TPM1_CH0) on PTB0 (FR motor speed)
+	PORTB->PCR[0] |= PORT_PCR_MUX(3); // enable ALT3 (TPM1_CH0) on PTB0 (Left Speed)
 	
 	PORTB->PCR[1] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[1] |= PORT_PCR_MUX(3); // enable ALT3 (TPM1_CH1) on PTB1 (FL motor speed)
+	PORTB->PCR[1] |= PORT_PCR_MUX(3); // enable ALT3 (TPM1_CH1) on PTB1 (Right Speed)
 	
 	PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[2] |= PORT_PCR_MUX(3); // enable ALT3 (TPM2_CH0) on PTB2 (BR motor speed)
+	PORTB->PCR[2] |= PORT_PCR_MUX(3); // enable ALT3 (TPM2_CH0) on PTB2 (Left Direction)
 	
 	PORTB->PCR[3] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[3] |= PORT_PCR_MUX(3); // enable ALT3 (TPM2_CH1) on PTB3 (BL Motor Speed)
+	PORTB->PCR[3] |= PORT_PCR_MUX(3); // enable ALT3 (TPM2_CH1) on PTB3 (Right Direction)
 	
 	// configure GPIO pins
-	PORTD->PCR[0] = PORT_PCR_MUX(1); // PTD0 - FR Motor DIR1
-	PORTD->PCR[1] = PORT_PCR_MUX(1); // PTD1 - FR Motor DIR2
-	PORTD->PCR[2] = PORT_PCR_MUX(1); // PTD2 - FL Motor DIR1
-	PORTD->PCR[3] = PORT_PCR_MUX(1); // PTD3 - FL Motor DIR2
-	PORTD->PCR[4] = PORT_PCR_MUX(1); // PTD4 - BR Motor DIR1
-	PORTD->PCR[5] = PORT_PCR_MUX(1); // PTD5 - BR Motor DIR2
-	PORTD->PCR[6] = PORT_PCR_MUX(1); // PTD6 - BL Motor DIR1
-	PORTD->PCR[7] = PORT_PCR_MUX(1); // PTD7 - BL Motor DIR2
+	//PORTD->PCR[0] = PORT_PCR_MUX(1); // PTD0 - FR Motor DIR1
+	//PORTD->PCR[1] = PORT_PCR_MUX(1); // PTD1 - FR Motor DIR2
+	//PORTD->PCR[2] = PORT_PCR_MUX(1); // PTD2 - FL Motor DIR1
+	//PORTD->PCR[3] = PORT_PCR_MUX(1); // PTD3 - FL Motor DIR2
+	//PORTD->PCR[4] = PORT_PCR_MUX(1); // PTD4 - BR Motor DIR1
+	//PORTD->PCR[5] = PORT_PCR_MUX(1); // PTD5 - BR Motor DIR2
+	//PORTD->PCR[6] = PORT_PCR_MUX(1); // PTD6 - BL Motor DIR1
+	//PORTD->PCR[7] = PORT_PCR_MUX(1); // PTD7 - BL Motor DIR2
 	
 	// Set GPIO Direction Pins as OUTPUTS
-	PTD->PDDR |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+	//PTD->PDDR |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
 		
 	// set PWM period
 	TPM1->MOD = 7500;
@@ -63,13 +70,15 @@ void initMotor(){
 		
 	// clear the CMOD bits to diable the timer, and clear the prescalar bits
 	//TPM1->SC &= ~TPM_SC_CMOD_MASK;
-	TPM1->SC &= ~TPM_SC_PS_MASK;
+	//TPM1->SC &= ~TPM_SC_PS_MASK;
 	
 	//TPM2->SC &= ~TPM_SC_CMOD_MASK;
-	TPM2->SC &= ~TPM_SC_PS_MASK;
+	//TPM2->SC &= ~TPM_SC_PS_MASK;
 	
-	// set prescalar to 32
+	// clear prescaler and set prescalar to 32
+	TPM1->SC &= ~TPM_SC_PS_MASK;
 	TPM1->SC |=  TPM_SC_PS(5);
+	TPM2->SC &= ~TPM_SC_PS_MASK;
 	TPM2->SC |=  TPM_SC_PS(5);
 	
 	// Step 3. Set TPM to Edge-Alighned PWM Mode
@@ -126,113 +135,118 @@ void motorControl(int state, int speed){
 	// DIR1 == 1 for forward, DIR2 == 1 for reverse
 	
 	// Convert speed (0-100) to duty cycle
-	int dutyCycle = (speed * 7500) / 100;
+	int dutyCycle = (speed * 7500) / 100; // need to change this base on wuming's angle input
 	
 	switch(state){
+		
 		case forward:
 			// Set direction to forward (DIR1 = HIGH, DIR2 = LOW)
-			PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
-			PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
+			//PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
+			//PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_HIGH; // Left Direction
+			TPM2_C1V = PWM_HIGH; // Right Direction
 			break;
+		
 		case rightTurnOnSpot:
-			
 			// right motors shld be in reverse aka DIR2, left motors shld be in forward aka DIR1
-			PTD->PSOR = (1 << 1) | (1 << 2) | (1 << 5) | (1 << 6);
-			PTD->PCOR = (1 << 0) | (1 << 3) | (1 << 4) | (1 << 7); 
+			//PTD->PSOR = (1 << 1) | (1 << 2) | (1 << 5) | (1 << 6);
+			//PTD->PCOR = (1 << 0) | (1 << 3) | (1 << 4) | (1 << 7); 
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_HIGH; // Right Direction
 			break;
+		
 		case reverse:
 			// Set direction to reverse (DIR1 = LOW, DIR2 = HIGH)
-			PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
-			PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
-
+			//PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
+			//PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
 			
 		case leftTurnOnSpot:
 			// left motors shld be in reverse aka DIR2, right motors shld be in forward aka DIR1
-			PTD->PSOR = (1 << 0) | (1 << 3) | (1 << 4) | (1 << 7);
-			PTD->PCOR = (1 << 1) | (1 << 2) | (1 << 5) | (1 << 6); 
+			//PTD->PSOR = (1 << 0) | (1 << 3) | (1 << 4) | (1 << 7);
+			//PTD->PCOR = (1 << 1) | (1 << 2) | (1 << 5) | (1 << 6); 
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_HIGH; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
+		
 		case rightTurnForward:
 			// since still moving forward GPIO shld be same as forward
-			PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
-			PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
+			//PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
+			//PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle - x; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle - x; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle - x; // Right Speed
+			TPM2_C0V = PWM_HIGH; // Left Direction
+			TPM2_C1V = PWM_HIGH; // Right Direction
 			break;
+		
 		case leftTurnForward:
 			// since still moving forward GPIO shld be same as forward
-			PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
-			PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
+			//PTD->PSOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 HIGH
+			//PTD->PCOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 LOW
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle - x; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle - x; // BL Motor
+			TPM1_C0V = dutyCycle - x; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_HIGH; // Left Direction
+			TPM2_C1V = PWM_HIGH; // Right Direction
 			break;
+		
 		case rightTurnReverse:
 			// since still moving in reverse direction, GPIO shld be same as reverse
-			PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
-			PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
+			//PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
+			//PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle - x; // FR Motor
-			TPM1_C1V = dutyCycle; // FL Motor
-			TPM2_C0V = dutyCycle - x; // BR Motor
-			TPM2_C1V = dutyCycle; // BL Motor
+			TPM1_C0V = dutyCycle; // Left Speed
+			TPM1_C1V = dutyCycle - x; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
 			
 		case leftTurnReverse:
 			// since still moving in reverse direction, GPIO shld be same as reverse
-			PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
-			PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
+			//PTD->PCOR = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 6); // DIR1 LOW
+			//PTD->PSOR = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7); // DIR2 HIGH
 			// Apply speed (PWM)
-			TPM1_C0V = dutyCycle; // FR Motor
-			TPM1_C1V = dutyCycle - x; // FL Motor
-			TPM2_C0V = dutyCycle; // BR Motor
-			TPM2_C1V = dutyCycle - x; // BL Motor
+			TPM1_C0V = dutyCycle - x; // Left Speed
+			TPM1_C1V = dutyCycle; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
+		
 		case brake:
 			// everything set to 0
-			PTD->PCOR = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) |
-									(1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+			//PTD->PCOR = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) |
+									//(1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
 			// Apply speed (PWM)
-			TPM1_C0V = 0; // FR Motor
-			TPM1_C1V = 0; // FL Motor
-			TPM2_C0V = 0; // BR Motor
-			TPM2_C1V = 0; // BL Motor
+			TPM1_C0V = PWM_LOW; // Left Speed
+			TPM1_C1V = PWM_LOW; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
+		
 		default:
 			// everything set to 0
-			PTD->PCOR = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) |
-									(1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+			//PTD->PCOR = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) |
+									//(1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
 			// Apply speed (PWM)
-			TPM1_C0V = 0; // FR Motor
-			TPM1_C1V = 0; // FL Motor
-			TPM2_C0V = 0; // BR Motor
-			TPM2_C1V = 0; // BL Motor
+			TPM1_C0V = PWM_LOW; // Left Speed
+			TPM1_C1V = PWM_LOW; // Right Speed
+			TPM2_C0V = PWM_LOW; // Left Direction
+			TPM2_C1V = PWM_LOW; // Right Direction
 			break;
-			
 	}
 }
