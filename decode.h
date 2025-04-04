33179ac7634fe69d0ec29bc736220cf56d66d291
Neo Jie Ming, Kin <142193380+kinneo@users.noreply.github.com>
2025-04-04 20:50:54 +0800

    Update decode.h

--- decode.h
+++ decode.h
@@ -1,6 +1,23 @@
 #include <MKL25Z4.h>
 #include "fetchData.h"
 //#include "motorControl.h"
+#include <stdbool.h>
+
+#define forward 0
+#define rightTurnOnSpot 1
+#define reverse 2
+#define leftTurnOnSpot 3
+#define rightTurnForward 4
+#define leftTurnForward 5
+#define rightTurnReverse 6
+#define leftTurnReverse 7
+#define brake 8
+
+volatile bool isComplete = false;
+volatile bool isMoving = false;
+volatile int state = 8;
+volatile int speed = 75;
+volatile uint8_t turnFactor = 0;
 
 typedef struct {
     uint8_t A : 3;
@@ -14,41 +31,48 @@ void decode(uint8_t raw_data){
     // Movement OP
     TPacket data;
     data.OP = (raw_data >> 6) & 0b11;
-    data.D  = (raw_data >> 5) & 0b01;
-    data.C  = (raw_data >> 4) & 0b01;
-    data.B  = (raw_data >> 3) & 0b01;
+    data.D  = (raw_data >> 5) & 0b1;
+    data.C  = (raw_data >> 4) & 0b1;
+    data.B  = (raw_data >> 3) & 0b1;
     data.A  = (raw_data) & 0b111;
     
     if(data.OP == 0b00){
-        int speed = (data.B) ? 100 : 75;
-        int turnFactor = data.A % 4;
-        int state;
+        speed = (data.B) ? 100 : 75;
+        turnFactor = data.A % 4;
         if (data.D){
             //forward with turn
             if (data.A){
                 state = data.A >> 2 ? rightTurnForward : leftTurnForward;
             } else {
+                //forward only
                state = forward;
             }
+            isMoving = true;
         } else if (data.C){
             if (data.A){
+                //reverse with turn
                 state = data.A >> 2 ? rightTurnReverse : leftTurnReverse;
             } else {
+                //reverse only
                 state = reverse;
             }
+            isMoving = true;
         } else if (data.A){
+            //turn on the spot
             state = data.A >> 2 ? rightTurnOnSpot : leftTurnOnSpot;
-						speed = 100;
+            isMoving = true;
         } else {
+            //not moving
             state = brake;
+            isMoving = false;
         }
-        motorControl(state,speed,turnFactor);
     }
 
     // Audio OP
     if(data.OP == 0b10){
         if(data.D){
             // complete function
+            isComplete = true;
         }
 
     }
